ac3a6eb857ac4eedf828a82f5083956884bce536
pIAyground <144573281+TornadoHM@users.noreply.github.com>
2024-04-08 17:59:05 -0600

    Lists.py

---
+++ lists.py
@@ -0,0 +1,58 @@
+# Variables de área (en metros cuadrados)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# información de la casa como lista de listas
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+# imprime house
+
+print(house)
+
+# Imprime el tipo de house
+
+print(type(house))
+
+# ------------------------------------------------------------------
+
+# variables de área (en metros cuadrados)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Modifica la lista areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Imprime areas
+
+print(areas)
+
+# -----------------------------------------------------------------
+
+# variables de área (en metros cuadrados)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Crea la lista areas
+
+areas = [hall,
+        kit,
+        liv,
+        bed,
+        bath]
+
+# Imprime areas
+
+print(areas)
