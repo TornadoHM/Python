361e24d757c565289185d3a2258b63120142156b
pIAyground <144573281+TornadoHM@users.noreply.github.com>
2024-04-08 17:15:36 -0600

    Compund value.py

---
+++ Compund value.py
@@ -0,0 +1,14 @@
+# Crea la variable savings
+savings = 100
+
+# Crea la variable growth_multiplier
+
+growth_multiplier = 1.1
+
+# Calcula result
+
+result = savings * growth_multiplier ** 7
+
+# Imprime result
+
+print(result)
