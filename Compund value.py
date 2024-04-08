3663976544b96c2f87680f3775ec6c953b4215a5
pIAyground <144573281+TornadoHM@users.noreply.github.com>
2024-04-08 17:42:24 -0600

    Update Compund value.py

--- Compund value.py
+++ Compund value.py
@@ -12,3 +12,25 @@ result = savings * growth_multiplier ** 7
 # Imprime result
 
 print(result)
+
+# ----------------------------------------------------------------
+
+# Definición de savings y result
+savings = 100
+str(savings)
+result = 100 * 1.10 ** 7
+str(result)
+# Arregla la impresión
+print("Empecé con $" + str(savings) + " y ahora tengo $" + str(result) + ".  ¡Impresionante!")
+
+# Definición de pi_string
+pi_string = "3.1415926"
+
+# Convierte pi_string en float: pi_float
+
+pi_float = float(pi_string)
+
+
+# There is different types of variables, and defining them has an impactfull meaning besides the code
+        # int() float() bool() str() 
+        # depending on the situation they take certain actions or others
