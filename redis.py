80c76328e695b19ee021acfc5e38e00f6b901292
pIAyground <144573281+TornadoHM@users.noreply.github.com>
2024-09-19 14:51:05 -0600

    Redis.py

---
+++ redis.py
@@ -0,0 +1,18 @@
+import socket
+
+def main():
+    
+    print("Logs from your program will appear here!")
+
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    client_socket, _ = server_socket.accept()  # wait for client
+    
+    while True:
+        request: bytes = client_socket.recv(512)
+        data: str = request.decode()
+        
+        if "ping" in data.lower():
+            client_socket.send("+PONG\r\n".encode())
+            break
+if __name__ == "__main__":
+    main()
