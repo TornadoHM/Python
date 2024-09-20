8592b36980d2ebdbd480aa49733434006ef79c1f
pIAyground <144573281+TornadoHM@users.noreply.github.com>
2024-09-20 16:17:12 -0600

    redis.py

--- redis.py
+++ redis.py
@@ -1,18 +1,43 @@
 import socket
+from typing import Tuple
+import threading
 
-def main():
-    
-    print("Logs from your program will appear here!")
+def handle_client(connection: socket.socket, address: Tuple[str, int]) -> None:
+    with connection:
+        print(f"Accepted connection from {address}\n")
+        while True:
+            data: bytes = connection.recv(1024)
+            if not data:
+                print(f"Connection closed by {address}")
+                break
+            if "ping" in data.decode().lower():
+                pong: str = "+PONG\r\n"
+                connection.sendall(pong.encode())
+            else:
+                # Handle other commands or provide a default response
+                response: str = "+OK\r\n"
+                connection.sendall(response.encode())
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _ = server_socket.accept()  # wait for client
-    
+def main() -> None:
+    print("Logs from your program will appear here!\n")
+    server_socket: socket.socket = socket.create_server(
+        ("localhost", 6379), reuse_port=True
+    )
+    print("Server started on localhost:6379")
+
     while True:
-        request: bytes = client_socket.recv(512)
-        data: str = request.decode()
-        
-        if "ping" in data.lower():
-            client_socket.send("+PONG\r\n".encode())
-            break
+        try:
+            connection: socket.socket
+            address: Tuple[str, int]
+            connection, address = server_socket.accept()
+
+            # Start a new thread for each client
+            client_thread = threading.Thread(
+                target=handle_client, args=(connection, address)
+            )
+            client_thread.start()
+        except Exception as e:
+            print(f"Exception: {e}")
+
 if __name__ == "__main__":
     main()
